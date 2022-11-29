7e8c2900c61652134c221d9cf4464bf72dda3df6
Paulo Sérgio <97705403+pauloelf@users.noreply.github.com>
2022-11-29 02:37:07 +0000

    projeto refatorado

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
 class Funcionamento {
-  constructor(save, modal) {
-    // Seletores html
+  constructor(modal, tbody) {
     this.selector = (item) => document.querySelector(item);
+    this.selectorId = (id) => document.getElementById(id);
     this.modal = this.selector(modal);
-    this.save = this.selector(save);
-    this.tbody = this.selector('tbody');
-    
-    this.seletorId = (id) => document.getElementById(id);
-    this.sNome = this.seletorId('$nome');
-    this.sFuncao = this.seletorId('$funcao');
-    this.sSalario = this.seletorId('$salario');
+    this.tbody = this.selector(tbody);
 
-    // Geral
-    this.ativo = 'active';
-    this.itens = '';
-    this.id = '';
+    this.save = this.selectorId('$btnSalvar');
+    this.add = this.selectorId('new');
+    this.sNome = this.selectorId('$nome');
+    this.sFuncao = this.selectorId('$funcao');
+    this.sSalario = this.selectorId('$salario');
 
-    // BD LocalStorage
     this.getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? [];
     this.setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(this.itens));
+    this.ativo = 'active';
+    this.itens = '';
   }
 
-  initialInfo(index) {
-    this.i = index.path[3].querySelectorAll('td');
-    this.j = document.querySelector('tbody');
-    this.g = Array.from(this.j.querySelectorAll('tr'));
-
-    this.number = this.g.map((v, n) => {
-      let valor = v.querySelectorAll('td')[0].innerHTML == this.i[0].innerHTML;
-      if (valor) {
-        return n
-      }
-    })
+  addItem(index) {
+    this.openModal(false, index);
+  }
+  
+  editItem(index) {
+    this.openModal(true, index);
+  }
+  
+  deleteItem(index) {
+    this.getItens(index);
+    this.itens.splice(this.index, 1);
+    this.setItensBD();
+    this.loadItens();
+    this.addEventsButtons();
   }
 
   openModal(edit, index) {
@@ -43,41 +42,37 @@ class Funcionamento {
     }
 
     if (edit) {
-      this.initialInfo(index)
-      this.sNome.value = this.i[0].innerHTML;
-      this.sFuncao.value = this.i[1].innerHTML;
-      this.sSalario.value = parseInt(this.i[2].innerHTML.replace('R$', ''));
-      this.id = index;
+      this.getItens(index);
+      this.sNome.value = this.idx[0].innerHTML;
+      this.sFuncao.value = this.idx[1].innerHTML;
+      this.sSalario.value = parseInt(this.idx[2].innerHTML.replace('R$', ''));
+      this.edit = edit;
     } else {
       this.sNome.value = '';
       this.sFuncao.value = '';
       this.sSalario.value = '';
-      this.id = undefined;
+      this.edit = edit;
      }
   }
 
-  addItem(index) {
-    var edit = false
-    this.openModal(edit, index)
-  }
-  
-  editItem(index) {
-    var edit = true
-    this.openModal(edit, index)
-  }
-  
-  deleteItem(index) {
-    this.initialInfo(index)
-    let posicaoRemove = this.number.map(x => {
-      if (x !== undefined) {
-        return x
-      }
-    })
+  saveItem(e) {
+    if (this.sNome.value == '' || this.sFuncao.value == '' || this.sSalario.value == '') {
+      return
+    };
+    e.preventDefault();
+    
+    if (this.edit) {
+      this.itens[this.index].nome = this.sNome.value;
+      this.itens[this.index].funcao = this.sFuncao.value;
+      this.itens[this.index].salario = this.sSalario.value;
+    } else {
+      this.itens.push({'nome': this.sNome.value, 'funcao': this.sFuncao.value, 'salario': this.sSalario.value});
+    };
 
-    this.itens.splice(posicaoRemove.sort()[0], 1);
     this.setItensBD();
+    this.modal.classList.remove(this.ativo);
     this.loadItens();
-    this.addEventsButtons()
+    this.addEventsButtons();
   }
   
   insertItem(item, index) {
@@ -95,44 +90,29 @@ class Funcionamento {
     `;
     this.tbody.appendChild(tr);
   }
-  
-  saveItem(e) {
-    if (this.sNome.value == '' || this.sFuncao.value == '' || this.sSalario.value == '') {
-      return
-    };
-    e.preventDefault();
-    
-    if (this.id !== undefined) {
-      let posicao = this.number.map(x => {
-        if (x !== undefined) {
-          return x
-        }
-      })
-      
-      this.itens[posicao.sort()[0]].nome = this.sNome.value;
-      this.itens[posicao.sort()[0]].funcao = this.sFuncao.value;
-      this.itens[posicao.sort()[0]].salario = this.sSalario.value;
-    } else {
-      this.itens.push({'nome': this.sNome.value, 'funcao': this.sFuncao.value, 'salario': this.sSalario.value})
-    };
 
-    this.setItensBD();
-    this.modal.classList.remove(this.ativo);
-    this.loadItens();
-    this.id = undefined;
-    this.addEventsButtons()
+  getItens(index) {
+    this.idx = index.path[3].querySelectorAll('td');
+    if (this.idx.length > 5) {
+      this.idx = index.path[2].querySelectorAll('td')
+    }
+    this.itens.forEach((item, index) => {
+      if(item.nome == this.idx[0].innerHTML) {
+        this.index = index;
+        this.item = item;
+      };
+    });
   }
   
   addEventsButtons() {
-    this.add = this.selector('#new');
     this.edit = document.querySelectorAll('.edit');
     this.remove = document.querySelectorAll('.remove');
     this.add.addEventListener('click', this.addItem);
-    this.save.addEventListener('click', this.saveItem)
+    this.save.addEventListener('click', this.saveItem);
 
     if (this.remove !== null || this.edit !== null) {
-      this.edit.forEach(i => i.addEventListener('click', this.editItem))
-      this.remove.forEach(i => i.addEventListener('click', this.deleteItem))
+      this.edit.forEach(i => i.addEventListener('click', this.editItem));
+      this.remove.forEach(i => i.addEventListener('click', this.deleteItem));
     }
   }
   
@@ -153,12 +133,12 @@ class Funcionamento {
   }
   
   init() {
-    this.loadItens()
-    this.bindEvents()
-    this.addEventsButtons()
+    this.loadItens();
+    this.bindEvents();
+    this.addEventsButtons();
     return this
   }
 }
 
-const funcionamento = new Funcionamento('#btnSalvar', '.modal-container')
-funcionamento.init()
+const funcionamento = new Funcionamento('.modal-container', 'tbody');
+funcionamento.init();
